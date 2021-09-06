aa79e6097e890e519ace5034b6a162d06c91dd32
Manas Bommakanti <mbommaka@data.cs.purdue.edu>
2021-09-06 11:39:15 -0400

    Done!

--- answers.sh
+++ answers.sh
@@ -4,42 +4,47 @@
 cd ORIGIN
 
 # Task 1 
-
+mv Homework/MA165/HW2.txt Homework/MA165/HW1.txt
 
 # Task 2
-
+# rm Memes/Spongebob/money.jpeg
 
 # Task 3
-
+mv Memes/Star\ Wars/fafsa.jpg Memes/Spongebob/fafsa.jpg
 
 # Task 4
-
+rm -r Homework/CS191
 
 # Task 5
-
+mkdir Programs/C++
 
 # Task 6
-
+mv Programs/Age.cpp Programs/C++/Age.cpp
 
 # Task 7
-
+cp Programs/Java Homework/CS180/Project_1
 
 # Task 8
-
+mv Programs/*.c Programs/C
 
 # Task 9
-
+mv Programs/*.py Programs/Python\ 3
 
 # Task 10
-
+touch Homework/POL141/Essay.txt
 
 # Task 11
-
+ls -al
 
 # Task 12
-: '
+:set mouse=a
+:colorscheme darkblue
+:set autoindent expandtab
+' 
 Type your vim/nano customizations below:
-
+1. Enables mouse support
+2. Changes color scheme to dark blue
+3. Increases indent size
 
 
 '
